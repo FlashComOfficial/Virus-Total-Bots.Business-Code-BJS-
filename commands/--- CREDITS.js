2d2eb6be2574d32b176daeb46fdd54965daa9b39
Bots.Business <git.sync@bots.business>
2025-08-27 16:02:53 +0000

    Bots.Business export for bot id: 2469744

---
+++ commands/--- CREDITS.js
@@ -0,0 +1,33 @@
+/*CMD
+  command: --- CREDITS
+  help: 
+  need_reply: false
+  auto_retry_time: 
+  folder: 
+
+  <<ANSWER
+
+  ANSWER
+
+  <<KEYBOARD
+
+  KEYBOARD
+  aliases: 
+  group: 
+CMD*/
+
+📢 Credits Notice  
+
+This bot and its code were created and developed by FlashCom,  
+with the assistance of AI (For Time Saving )  
+
+All logic, VirusTotal integration, and functionality have been tailored  
+specifically for use on the **Bots.Business** platform.  
+
+🤖 Powered by FlashCom  
+✨ Made with ❤️ by AI
+
+Any bugs report to : https://t.me/flashcomassistant
+
+For more codes bots : https://getmart.iblogger.org
+
